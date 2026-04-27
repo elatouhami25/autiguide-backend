cc3accb2f73a41aeb32cfadc21fd92ae7c95ee3a
Ela Thami <elatouhami25@gmail.com>
2026-04-27 23:05:10 +0100

    Initial commit - Spring Boot AutiGuide

---
+++ src/main/java/com/autiguide/autiguide/AutiguideApplication.java
@@ -0,0 +1,13 @@
+package com.autiguide.autiguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutiguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutiguideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autiguide/autiguide/AutiguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autiguide.autiguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutiguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
