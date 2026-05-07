93a14036efa36ca959be02e1e82d60d5e8f718c3
Ela Thami <elatouhami25@gmail.com>
2026-05-07 21:44:24 +0100

    Ajout nouvelle fonctionnalité

--- src/main/java/com/autiguide/autiguide/service/RessourceService.java
+++ src/main/java/com/autiguide/autiguide/service/RessourceService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-class RessourceService {
+public class RessourceService {
 
     private final RessourceRepository ressourceRepository;
 
